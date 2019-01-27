681bcc71a5209928babc0817b1dda0d69d0f9602
Thiago Fernandes Cerqueira <tfcerqueira@MacBook-Air-de-Thiago.local>
2019-01-27 00:05:05 -0200

    Add project page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
 // /bad - when a request fails - send back json with errorMessage
 
 app.get('/bad', (req, res) => {
